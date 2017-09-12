82cea014b6b57dcb163e39b3cf3e9453b442dce0
pavelperc <31820550+pavelperc@users.noreply.github.com>
2017-09-12 19:14:47 +0300

    Просто первые наброски

--- app/src/main/java/com/perc/pavel/sportgeolocationgame/RegistrationActivity.java
+++ app/src/main/java/com/perc/pavel/sportgeolocationgame/RegistrationActivity.java
@@ -1,26 +1,28 @@
 package com.perc.pavel.sportgeolocationgame;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText etLogin;
+    EditText etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        etLogin = (EditText) findViewById(R.id.etLogin);
+        etPassword = (EditText) findViewById(R.id.etPassword);
     }
 
     @Override
@@ -32,20 +34,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        
+
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-    
-    
-    public void btnLoginClick(View v){
-        
-    }
+
     public void btnRegistrationClick(View v){
-        Intent intent = new Intent(this, RegistrationActivity.class);
-        startActivity(intent);
+        String login = etLogin.getText().toString();
+        String password = etPassword.getText().toString();
+
+        Toast.makeText(this, "login = " + login + "\npassword = " + password, Toast.LENGTH_SHORT).show();
     }
 }
