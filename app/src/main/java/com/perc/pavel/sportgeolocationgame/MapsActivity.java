de4383294aeb89e815dea41a87378ab7343ee1d3
pavelperc <31820550+pavelperc@users.noreply.github.com>
2017-09-24 21:14:15 +0300

    Добавление отслеживания местоположения.

--- app/src/main/java/com/perc/pavel/sportgeolocationgame/MapsActivity.java
+++ app/src/main/java/com/perc/pavel/sportgeolocationgame/MapsActivity.java
@@ -1,19 +1,51 @@
 package com.perc.pavel.sportgeolocationgame;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
-
+    private FusedLocationProviderClient mFusedLocationClient;
+    
+    //private Location myLastLocation = null;
+    
+    private LocationSource.OnLocationChangedListener locationChangeListener = null;
+    
+    
+    private final LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location location = locationResult.getLastLocation();
+            // Запускаем locationChangeListener
+            if (locationChangeListener != null) {
+                locationChangeListener.onLocationChanged(location);
+            }
+            
+            
+        };
+    };
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +54,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
+        
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        startLocationUpdates();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationUpdates();
+    }
 
+    private void startLocationUpdates() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(3000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        try {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        } catch (SecurityException ignored){};
+    }
+    
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+    
+    
+    
+    
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,10 +95,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        
+        mMap.setLocationSource(new LocationSource() {
+            @Override
+            public void activate(OnLocationChangedListener onLocationChangedListener) {
+//                try {
+//                    onLocationChangedListener.onLocationChanged(mFusedLocationClient.getLastLocation().getResult());
+//                } catch (SecurityException ignored){}
+                
+                locationChangeListener = onLocationChangedListener;
+            }
+            
+            @Override
+            public void deactivate() {
+                locationChangeListener = null;
+            }
+        });
+        
+        
+        try {
+            mMap.setMyLocationEnabled(true);
 
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    //Toast.makeText(MapsActivity.this, "Moving camera", Toast.LENGTH_SHORT).show();
+                    LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 18.0f));// 2,0 - 21.0
+                }
+            });
+            
+        } catch (SecurityException ignored){}
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        
     }
 }
