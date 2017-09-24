02fdd39cb9aa5a8d262ff5f595fc551364581c1e
pavelperc <31820550+pavelperc@users.noreply.github.com>
2017-09-24 21:13:14 +0300

    Запрос и проверка всех разрешений.

--- app/src/main/java/com/perc/pavel/sportgeolocationgame/EnterActivity.java
+++ app/src/main/java/com/perc/pavel/sportgeolocationgame/EnterActivity.java
@@ -1,6 +1,15 @@
 package com.perc.pavel.sportgeolocationgame;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +18,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,7 +28,7 @@ import org.json.JSONObject;
  * Вход
  */
 public class EnterActivity extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST = 1;
     EditText etLogin;
     EditText etPassword;
     ProgressBar pbLoading;
@@ -29,9 +41,63 @@ public class EnterActivity extends AppCompatActivity {
         etLogin = (EditText) findViewById(R.id.etLogin);
         etPassword = (EditText) findViewById(R.id.etPassword);
         pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
+
+        // запрашиваем разрешение на местоположение
+        askPermissions();
+        checkGoogleServices();
     }
-    
-    
+
+    /**
+     * Проверка наличия актуальных сервисов гугл на устройстве. (Для gps)
+     */
+    private void checkGoogleServices() {
+        GoogleApiAvailability instance = GoogleApiAvailability.getInstance();
+        int res = instance.isGooglePlayServicesAvailable(this);
+
+        if (res != ConnectionResult.SUCCESS) {
+            instance.getErrorDialog(this, res, 0).create();
+        }
+    }
+
+    /**
+     * Запрос разрешения на использование геолокации, если оно ещё не получено и если версия андроида > 6.0 
+     */
+    protected void askPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+        }
+    }
+
+    /**
+     * Обработка ответа на запрос разрешения на использование геолокации.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    
+                    // Создаём диалоговое окно об ошибке доступа.
+                    new AlertDialog.Builder(this)
+                            .setTitle("Ошибка доступа к геолокации.")
+                            .setMessage("Приложение будет остановлено.")
+                            .setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    EnterActivity.this.finish();
+                                }
+                            }).create().show();
+                }
+            }
+        }
+    }
+
     public void btnEnterClick(View v) {
         String login = etLogin.getText().toString();
         String password = etPassword.getText().toString();
